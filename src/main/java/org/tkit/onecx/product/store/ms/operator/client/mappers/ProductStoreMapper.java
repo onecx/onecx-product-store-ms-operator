76ab7aee672c62f26d435c566da84b81646350f1
Andrej Petras <andrej@ajka-andrej.com>
2024-04-16 17:20:39 +0200

    feat: add ms undeployed attribute (#10)
    * feat: add ms undeployed attribute
    
    * feat: add ms undeployed attribute

--- src/main/java/org/tkit/onecx/product/store/ms/operator/client/mappers/ProductStoreMapper.java
+++ src/main/java/org/tkit/onecx/product/store/ms/operator/client/mappers/ProductStoreMapper.java
@@ -1,6 +1,7 @@
 package org.tkit.onecx.product.store.ms.operator.client.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.tkit.onecx.product.store.ms.operator.MicroserviceSpec;
 
 import gen.org.tkit.onecx.product.store.ms.v1.model.UpdateMsRequest;
@@ -8,6 +9,7 @@ import gen.org.tkit.onecx.product.store.ms.v1.model.UpdateMsRequest;
 @Mapper
 public interface ProductStoreMapper {
 
+    @Mapping(target = "undeployed", constant = "false")
     UpdateMsRequest map(MicroserviceSpec spec);
 
 }
